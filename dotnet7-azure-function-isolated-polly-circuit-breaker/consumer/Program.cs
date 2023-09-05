3a7bfceefaa5cdf3d17554dc93c14cf56d59eb00
Marcel Medina <marcel.net@gmail.com>
2023-09-06 00:00:02 +1200

    dotnet7-azure-function-isolated-polly-circuit-breaker

---
+++ dotnet7-azure-function-isolated-polly-circuit-breaker/consumer/Program.cs
@@ -0,0 +1,42 @@
+using consumer.TypedHttpClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Polly;
+
+var host = new HostBuilder()
+    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureAppConfiguration((hostingContext, config) =>
+    {
+        var currentDirectory = hostingContext.HostingEnvironment.ContentRootPath;
+
+        config.SetBasePath(currentDirectory)
+            .AddJsonFile("settings.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables();
+        config.Build();
+    })
+    .ConfigureServices((services) =>
+    {
+        var circuitBreakPolicy = Policy
+            .HandleResult<HttpResponseMessage>(response => !response.IsSuccessStatusCode)
+            .CircuitBreakerAsync(3, 
+                TimeSpan.FromSeconds(10),
+                onBreak: (_, _) =>
+                {
+                    Console.Out.WriteLine("*****Open*****");
+                },
+                onReset: () =>
+                {
+                    Console.Out.WriteLine("*****Closed*****");
+                },
+                onHalfOpen: () =>
+                {
+                    Console.Out.WriteLine("*****Half Open*****");
+                });
+
+        services.AddHttpClient<StateCounterHttpClient>()
+            .AddPolicyHandler(circuitBreakPolicy);
+    })
+    .Build();
+
+host.Run();
